b06d7b90351dbe1217fb5278049525d582709392
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-24 15:39:33 -0700

    Add three input fields to HTML for grocery items
      Co-authored-by: Auston Moos <Austonhmoos2004@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// When page loads
+window.addEventListener("load", function(){
+  // When the user submits the form
+  document.addEventListener("submit", function(event){
+    // Hide the form
+    // Display the list of items in alphabetical order, all uppercased
+  });
+
+});
